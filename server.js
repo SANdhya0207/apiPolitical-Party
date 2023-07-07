ca50d771903023ca60da2b86e380fd2a49a3414b
SANdhya0207 <jhas54212@gmail.com>
2023-07-07 22:21:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+const cors = require ("cors")
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const Port = process.env.PORT || 5000;
+
+app.get('/', (req,res) => {
+      res.send("Api is running");
+})
+
+app.listen(Port, () => {
+      console.log(`Server running on port ${Port}`)
+})
