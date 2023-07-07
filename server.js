9e18e0ef3afff372f078663f1950f74aa650bced
SANdhya0207 <114745280+SANdhya0207@users.noreply.github.com>
2023-07-07 22:35:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require ("express");
 const cors = require ("cors")
 const app = express();
+const data = require("./api.json")
 
 app.use(express.json());
 app.use(cors());
 
 const Port = process.env.PORT || 5000;
 
+
 app.get('/', (req,res) => {
       res.send("Api is running");
+})
+
+app.get('/api', (req,res) => {
+      res.send(data);
 })
 
 app.listen(Port, () => {
